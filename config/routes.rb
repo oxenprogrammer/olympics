c91cea91fe57c1730b5dff56d9e66ac79c0e467b
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-19 22:51:27 +0300

    Feat(route): add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'sessions/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :users
 end
