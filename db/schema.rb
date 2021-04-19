9a8cbc23d36ec36df026cbc21a62640bc207f114
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-19 22:27:13 +0300

    Feat(model): update article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_191918) do
+ActiveRecord::Schema.define(version: 2021_04_19_192542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,12 +19,12 @@ ActiveRecord::Schema.define(version: 2021_04_19_191918) do
     t.string "title"
     t.text "text"
     t.string "image"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_articles_on_author_id"
     t.index ["category_id"], name: "index_articles_on_category_id"
-    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -41,5 +41,5 @@ ActiveRecord::Schema.define(version: 2021_04_19_191918) do
   end
 
   add_foreign_key "articles", "categories"
-  add_foreign_key "articles", "users"
+  add_foreign_key "articles", "users", column: "author_id"
 end
