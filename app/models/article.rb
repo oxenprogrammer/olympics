ff13d051913ea9a8711d5dbb8cfe137df1efc7b5
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-19 22:38:18 +0300

    Feat(model): add article association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :category
+  has_many :votes, dependent: :destroy
+  has_many :voters, through: :votes, source: :user
 end
