050532a45cbc496b91a419e9fcd20c4591d3d554
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-20 15:18:35 +0300

    Feat(controller): add create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      log_in @user
+      flash[:success] = 'Welcome to Olympics Articles'
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
