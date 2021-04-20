b902d6d026efae853211756b2d3eb663477573a4
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-20 15:30:30 +0300

    Feat(model): add password digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_122333) do
+ActiveRecord::Schema.define(version: 2021_04_20_122903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2021_04_20_122333) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "password_digest"
   end
 
   create_table "votes", force: :cascade do |t|
