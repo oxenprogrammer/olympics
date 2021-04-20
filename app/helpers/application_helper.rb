871125e26869ad644f8a90a33c3d82e65d280353
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-20 16:56:45 +0300

    Feat(view): update navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,6 @@ module ApplicationHelper
     link_to current_user.name, user_url(current_user.id), class: 'nav-link' if logged_in?
   end
 
-  def navbar_link_new_event
-    link_to 'Add New Event', events_path, class: 'text-white btn btn-success' if logged_in?
-  end
-
   def navbar_link_logout
     link_to 'Logout', logout_path, method: :delete, class: 'nav-link' if logged_in?
   end
@@ -19,11 +15,11 @@ module ApplicationHelper
     link_to 'Login', login_path, class: 'nav-link' unless logged_in?
   end
 
-  def navbar_link_home
-    if logged_in?
-      link_to 'Private Events', events_all_url, class: 'navbar-brand'
-    else
-      link_to 'Private Events', root_url, class: 'navbar-brand'
-    end
-  end
+  # def navbar_link_home
+  #   if logged_in?
+  #     link_to 'Private Events', events_all_url, class: 'navbar-brand'
+  #   else
+  #     link_to 'Private Events', root_url, class: 'navbar-brand'
+  #   end
+  # end
 end
