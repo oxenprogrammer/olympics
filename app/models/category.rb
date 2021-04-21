185d47061408f50083c86001644b1dc9a2f3384c
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-22 00:12:47 +0300

    Feat(model): add priority method

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,20 @@
 class Category < ApplicationRecord
   has_many :articles
   enum name: %i[athletics soccer board_games swimming]
+
+  validates :name, presence: true
+
+  after_initialize :init
+
+  def init
+    self.priority ||= Category.maximum('priority').nil? ? 1 : Category.maximum('priority') + 1
+  end
+
+  def self.prioritize(votes_by_category)
+    categories = votes_by_category.keys
+    categories.each_with_index do |category, i|
+      find(category).update(priority: i + 1)
+    end
+    all
+  end
 end
