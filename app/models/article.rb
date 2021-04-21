a69e940c4e358986c2c1c9e4b216cade0ed6bf39
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-22 00:21:32 +0300

    Feat(model): add Article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,10 @@ class Article < ApplicationRecord
   belongs_to :category
   has_many :votes, dependent: :destroy
   has_many :voters, through: :votes, source: :user
+
+  validates :title, presence: true, length: { minimum: 6, maximum: 50 }
+  validates :text, presence: true, length: { minimum: 50, maximum: 3000 }
+  validates :author_id, presence: true
+  validates :category_id, presence: true
+  validates :image, format: { with: /\.(png|jpg|jpeg|webp)/, message: 'must be a URL for Webp, GIF, JPG or PNG image.' }
 end
