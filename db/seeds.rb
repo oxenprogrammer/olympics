4cd503ea6193d32e94c087b70005c2bc84ef640f
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-21 20:47:14 +0300

    Perf(database): add category seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.destroy_all
+Category.create!([{
+                   name: 'athletics'
+                 },
+                  {
+                    name: 'soccer'
+                  },
+                  {
+                    name: 'board_games'
+                  },
+                  {
+                    name: 'swimming'
+                  }])
+
+p 'Categories created successfully'
