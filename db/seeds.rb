e47af8203dc87b19a61af4a8c7a9aae64d460137
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-21 23:57:01 +0300

    Feat(database): add seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.destroy_all
+require 'faker'
+require 'database_cleaner'
+
+DatabaseCleaner.clean_with(:truncation)
+
+100.times do
+  User.create!(name: Faker::Name.unique.name, email: Faker::Internet.unique.email, password: '123456', password_confirmation: '123456')
+end
+
+p 'Users created Successfully'
+
 Category.create!([{
                    name: 'athletics'
                  },
@@ -21,3 +31,46 @@ Category.create!([{
                   }])
 
 p 'Categories created successfully'
+
+4.times do
+  Article.create!([{
+                    title: Faker::Lorem.sentence(word_count: 3),
+                    text: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
+                    image: 'https://images.freeimages.com/images/large-previews/eb7/mountain-1379228.jpg',
+                    author_id: rand(1..4),
+                    category_id: 1
+                  },
+                   {
+                     title: Faker::Lorem.sentence(word_count: 3),
+                     text: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
+                     image: 'https://images.freeimages.com/images/large-previews/fee/days-end-1392672.jpg',
+                     author_id: rand(1..4),
+                     category_id: 2
+                   },
+                   {
+                     title: Faker::Lorem.sentence(word_count: 3),
+                     text: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
+                     image: 'https://images.freeimages.com/images/large-previews/940/ostrich-masai-mara-kenya-1365799.jpg',
+                     author_id: rand(1..4),
+                     category_id: 3
+                   },
+                   {
+                     title: Faker::Lorem.sentence(word_count: 3),
+                     text: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
+                     image: 'https://images.freeimages.com/images/large-previews/600/bike-path-1438265.jpg',
+                     author_id: rand(1..4),
+                     category_id: 4
+                   }])
+end
+
+p 'Articles created successfully'
+
+100.times do |index|
+  Vote.create!({
+                 user_id: index + 1,
+                 article_id: rand(1..16)
+               })
+end
+
+p 'Random Votes created successfully'
+
