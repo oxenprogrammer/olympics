ba329db58058552f5a3367ab57668c4546277b34
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-22 15:23:54 +0300

    Feat(controller): add create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :require_login, only: %i[new create]
 
   def index
     @articles = Article.all
@@ -19,4 +20,27 @@ class ArticlesController < ApplicationController
   end
 
   def show; end
+
+  def create
+    @article = current_user.articles.build(article_params)
+
+    if @article.save
+      redirect_to article_url @article.id
+    else
+      render :new
+    end
+  end
+
+  def require_login
+    return unless current_user.nil?
+
+    flash[:alert] = 'Please login to Create an Article'
+    redirect_to login_path
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :text, :image, :category_id)
+  end
+
+  private :require_login
 end
