ffe2d32da4658d74d388aa075239eece94f721bb
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-22 23:10:51 +0300

    Feat(controller): update user redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to root_path
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = 'Welcome to Olympics Articles'
-      redirect_to @user
+      redirect_to root_path
     else
       render 'new'
     end
