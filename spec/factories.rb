245a2b04ce7a06b5d839ee18ca8e18d3b5e80d1c
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-23 00:27:11 +0300

    Test(Rspec): pending test

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,16 +8,23 @@ FactoryBot.define do
     password_confirmation { '123456' }
   end
 
+  factory :author, class: 'User' do
+    name { Faker::Name.unique.name }
+    email { Faker::Internet.unique.email }
+    password { '123456' }
+    password_confirmation { '123456' }
+  end
+
   factory :article do
-    title { Faker::Lorem.sentence(word_count: 2) }
-    text { Faker::Lorem.paragraph_by_chars(number: 15, supplemental: false) }
+    title { 'Hello Friends' }
+    text { 'This is really beginning to really nag me, why is it not really working, man can you save me' }
     image { 'https://www1.pictures.zimbio.com/gi/Swimming+15th+FINA+World+Championships+Day+iNq3C_4oNJ_x.jpg' }
     author_id { rand(1..4) }
     category_id { rand(1..4) }
   end
 
   factory :category do
-    name { Faker::Lorem.characters(number: 7) }
+    name { Faker::Lorem.characters(number: 10) }
     priority { 1 }
   end
 
