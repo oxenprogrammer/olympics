c7de8b3ca701edbc4e1230652d2bc9c7bb46259d
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-24 21:02:59 +0300

    Fix(review): remove logic from view

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,4 +1,12 @@
 module CategoriesHelper
+  def vote(current_user, article)
+    return unless logged_in?
+
+    link_to 'Vote', article_votes_path(article[:id]),
+            params: { vote: { user_id: current_user.id, article_id: article.id } },
+            disabled: voted?(current_user, article), method: :post
+  end
+
   def voted?(user, article)
     Vote.where(user_id: user.id, article_id: article.id).empty? ? false : true
   end
@@ -6,4 +14,6 @@ module CategoriesHelper
   def vote_count(article)
     Vote.where(article_id: article.id).count
   end
+
+  private :voted?
 end
