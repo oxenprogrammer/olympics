858c82fb1ed8d72c24914f0b06da5898fff3c6ff
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-25 12:10:03 +0300

    Test(model): update category tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  before(:all) do
-    @category = Category.create(name: 'athletics1', priority: 1)
-  end
+  let(:category) { FactoryGirl.create(:category) }
 
   it 'should have valid attributes' do
-    expect(@category).to be_valid
+    expect(category).to be_valid
   end
 
   it 'is not valid without an name' do
-    category = Category.new(name: nil)
-    expect(category).to_not be_valid
+    new_category = Category.new(name: nil)
+    expect(new_category).to_not be_valid
   end
 
   describe 'Associations' do
