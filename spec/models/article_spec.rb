f571cc2f0b8dbf9e360ca4d501d444e1d303572f
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-25 12:03:31 +0300

    Test(model): update article tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,18 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  before(:all) do
-    @user = User.create(name: 'hannah', email: 'hannah12@gmail.com', password: '123456',
-                        password_confirmation: '123456')
-    category = Category.create(name: 'board_games12', priority: 3)
-    @article = Article.create(title: 'This is an Article',
-                              text: 'dfghh hhhh jjjjj jjjjj uuuuuuu uuuuuu fddddd ggggg yyyyy ddddd dddd ddddddd',
-                              image: 'https://www.ivacy.com/blog/wp-content/uploads/2016/08/watch-soccer-olympics.jpg',
-                              author_id: @user.id, category_id: category.id)
-  end
+  let(:user) { FactoryGirl.create(:user) }
+  let(:category) { FactoryGirl.create(:category) }
+  let(:article) { FactoryGirl.create(:article, author_id: user.id, category_id: category.id) }
 
   it 'should have valid attributes' do
-    expect(@article).to be_valid
+    expect(article).to be_valid
   end
 
   it 'is not valid without an category' do
