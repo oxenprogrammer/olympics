c1ed036cadf2386d1c4565f1642ae0c6ba6afe6f
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-25 10:38:34 +0300

    Test(model): add more vote tests

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,14 +3,24 @@ require 'faker'
 
 RSpec.describe Vote, type: :model do
   before(:all) do
-    user = User.create(name: 'hannah', email: 'hannah@gmail.com', password: '123456', password_confirmation: '123456')
+    @user = User.create(name: 'hannah', email: 'hannah@gmail.com', password: '123456', password_confirmation: '123456')
     category = Category.create(name: 'soccer', priority: 3)
-    article = Article.create(title: 'This is an Article', text: 'dfghh hhhh jjjjj jjjjj uuuuuuu uuuuuu fddddd ggggg yyyyy ddddd dddd ddddddd', image: 'https://www.ivacy.com/blog/wp-content/uploads/2016/08/watch-soccer-olympics.jpg', author_id: user.id, category_id: category.id)
-    @vote = Vote.create(article_id: article.id, user_id: user.id)
+    @article = Article.create(title: 'This is an Article', text: 'dfghh hhhh jjjjj jjjjj uuuuuuu uuuuuu fddddd ggggg yyyyy ddddd dddd ddddddd', image: 'https://www.ivacy.com/blog/wp-content/uploads/2016/08/watch-soccer-olympics.jpg', author_id: @user.id, category_id: category.id)
+    @vote = Vote.create(article_id: @article.id, user_id: @user.id)
   end
 
   it 'should have valid attributes' do
     expect(@vote).to be_valid
+  end
+
+  it 'is not valid without a user' do
+    vote = Vote.new(user_id: nil, article_id: @article.id)
+    expect(vote).to_not be_valid
+  end
+
+  it 'is not valid without an article' do
+    vote = Vote.new(user_id: @user.id, article_id: nil)
+    expect(vote).to_not be_valid
   end
 
   describe 'Associations' do
